d7aaacdc8fb6eeeee106e380636998b430bb561a
ToldoDM <bertoldo.damiano@gmail.com>
2021-05-21 11:00:44 +0200

    modificato startnode

--- startnode.sh
+++ startnode.sh
@@ -1,1 +1,1 @@
-geth --networkid 4224 --mine --minerthreads 2 --datadir "." --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --allow-insecure-unlock --nat "any" --rpcapi "eth,web3,personal,net,miner" --unlock 0 --password ./password.sec --ipcpath "~/.ethereum/geth.ipc"
+geth --networkid 4224 --mine --miner.threads 2 --datadir "." --nodiscover --http --http.port "8545" --port "30303" --http.corsdomain "*" --allow-insecure-unlock --nat "any" --http.api "eth,web3,personal,net,miner" --unlock 0 --password ./password.sec --ipcpath "~/.ethereum/geth.ipc"
